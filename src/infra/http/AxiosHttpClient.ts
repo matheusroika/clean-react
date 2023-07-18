94e0a71eaa41d1bbd2430b0441b2e415f1c5a84c
Matheus Roika <matheusroika@hotmail.com>
2023-07-18 17:57:17 -0300

    feat: ensure AxiosHttpClient returns correct values on success

--- src/infra/http/AxiosHttpClient.ts
+++ src/infra/http/AxiosHttpClient.ts
@@ -1,15 +1,14 @@
 import axios from 'axios'
 import type { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
-import type { Account } from '@/domain/models/Account'
-import type { AuthParams } from '@/domain/useCases/Authentication'
 
-export class AxiosHttpClient implements HttpPostClient<AuthParams, Account> {
-  async post (params: HttpPostParams<AuthParams>): Promise<HttpResponse<Account>> {
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
     const { url, body } = params
-    await axios.post(url, body)
+    const httpResponse = await axios.post(url, body)
 
     return {
-      statusCode: 200
+      statusCode: httpResponse.status,
+      body: httpResponse.data
     }
   }
 }
