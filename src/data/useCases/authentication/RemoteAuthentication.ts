ab0a052fe5430ed6e6f45f833ba5cdef67ae8e9a
Matheus Roika <matheusroika@hotmail.com>
2023-07-18 03:00:57 -0300

    feat: ensure RemoteAuthentication calls HttpClient with correct URL

---
+++ src/data/useCases/authentication/RemoteAuthentication.ts
@@ -0,0 +1,20 @@
+import type { HttpPostClient } from 'data/protocols/HttpPostClient'
+import type { Account } from 'domain/models/Account'
+import type { AuthParams, Authentication } from 'domain/useCases/Authentication'
+
+export class RemoteAuthentication implements Authentication {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {}
+
+  async auth (params: AuthParams): Promise<Account> {
+    await this.httpPostClient.post(this.url)
+
+    return {
+      email: '',
+      name: '',
+      accessToken: ''
+    }
+  }
+}
