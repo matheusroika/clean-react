e4967c40b6e3b2ac37ccf7f159403c492c4316ef
Matheus Roika <matheusroika@hotmail.com>
2023-07-18 02:32:18 -0300

    feat: add Account model and Authentication useCase

---
+++ src/domain/useCases/Authentication.ts
@@ -0,0 +1,10 @@
+import type { Account } from 'domain/models/Account'
+
+export type AuthParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth: (params: AuthParams) => Promise<Account>
+}
