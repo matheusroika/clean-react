f28d06abcd3c82f49e7bab4c12525efc9612ba49
Matheus Roika <matheusroika@hotmail.com>
2023-07-18 17:22:12 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/AxiosHttpClient.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import type { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
+import type { Account } from '@/domain/models/Account'
+import type { AuthParams } from '@/domain/useCases/Authentication'
+
+export class AxiosHttpClient implements HttpPostClient<AuthParams, Account> {
+  async post (params: HttpPostParams<AuthParams>): Promise<HttpResponse<Account>> {
+    await axios(params.url)
+
+    return {
+      statusCode: 200
+    }
+  }
+}
