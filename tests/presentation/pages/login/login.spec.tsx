9ddb12926a05ca6d9b227dafa1f75cc5f9b47f3c
Matheus Roika <matheusroika@hotmail.com>
2023-07-21 09:48:44 -0300

    test: ensure Login page renders button as disabled on initial state

--- tests/presentation/pages/login/login.spec.tsx
+++ tests/presentation/pages/login/login.spec.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Login from '@/presentation/pages/login/login'
 
 describe('Login page', () => {
-  test('Should not render modalWrapper on initial state', () => {
-    const { queryByTestId } = render(<Login />)
-    const modalWrapper = queryByTestId('modalWrapper')
+  test('Should render correctly on initial state', () => {
+    render(<Login />)
+    const modalWrapper = screen.queryByTestId('modalWrapper')
     expect(modalWrapper).toBeNull()
+    const submitButton = screen.getByTestId<HTMLButtonElement>('submit')
+    expect(submitButton.disabled).toBe(true)
   })
 })
