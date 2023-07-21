c424a6fb1fbee5a9d52a3d7cb9664cbf5ab985ca
Matheus Roika <matheusroika@hotmail.com>
2023-07-21 17:51:00 -0300

    feat: ensure Login page shows correct status if email Validation succeeds

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -8,10 +8,18 @@ interface Props extends React.ComponentPropsWithoutRef<'input'> {
 const Input: React.FC<Props> = (props) => {
   const { setValue } = props
 
+  const getStatus = (): string => {
+    return props.title ? '🔴' : '🟢'
+  }
+
+  const getTitle = (): string => {
+    return props.title || 'Tudo certo!'
+  }
+
   return (
     <div className={styles.inputWrapper}>
       <input data-testid={props.name} {...props} onChange={(e) => { setValue(e.target.value) }}/>
-      <span data-testid={`${props.name}Status`} title={props.title}>🔴</span>
+      <span data-testid={`${props.name}Status`} title={getTitle()}>{getStatus()}</span>
     </div>
   )
 }
