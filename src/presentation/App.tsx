e8f9c7b5bc2e555ad006e0985301e382cbaa7d7a
Matheus Roika <matheusroika@hotmail.com>
2023-07-21 08:52:08 -0300

    feat: apply global styles to App

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import './styles/global.scss'
 
 const App = (): React.ReactElement => {
   return <RouterProvider router={router} />
