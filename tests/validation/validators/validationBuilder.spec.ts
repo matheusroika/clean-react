ce68c6df0303cc43248868fef3497455033d9e17
Matheus Roika <matheusroika@hotmail.com>
2023-07-23 12:42:31 -0300

    refactor: remove unnecessary code

--- tests/validation/validators/validationBuilder.spec.ts
+++ tests/validation/validators/validationBuilder.spec.ts
@@ -1,22 +1,10 @@
-import { ValidationBuilder } from '@/validation/validators/validationBuilder'
+import { ValidationBuilder as sut } from '@/validation/validators/validationBuilder'
 import { RequiredFieldValidation } from '@/validation/validators'
-
-type Sut = {
-  sut: ValidationBuilder
-}
-
-const makeSut = (fieldName: string): Sut => {
-  const sut = ValidationBuilder.field(fieldName)
-  return {
-    sut
-  }
-}
 
 describe('Validation Builder', () => {
   test('Should return RequiredFieldValidation', () => {
     const field = 'any_field'
-    const { sut } = makeSut(field)
-    const validations = sut.required().build()
+    const validations = sut.field(field).required().build()
     expect(validations.length).toBe(1)
     expect(validations[0]).toEqual(new RequiredFieldValidation(field))
   })
