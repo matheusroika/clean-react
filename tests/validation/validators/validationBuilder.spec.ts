0fbabe2ab424d49b9af00f50cd1d133e26ac0757
Matheus Roika <matheusroika@hotmail.com>
2023-07-23 12:12:38 -0300

    test: ensure ValidationBuilder returns RequiredFieldValidation

---
+++ tests/validation/validators/validationBuilder.spec.ts
@@ -0,0 +1,23 @@
+import { ValidationBuilder } from '@/validation/validators/validationBuilder'
+import { RequiredFieldValidation } from '@/validation/validators/requiredFieldValidation'
+
+type Sut = {
+  sut: ValidationBuilder
+}
+
+const makeSut = (fieldName: string): Sut => {
+  const sut = ValidationBuilder.field(fieldName)
+  return {
+    sut
+  }
+}
+
+describe('Validation Builder', () => {
+  test('Should return RequiredFieldValidation', () => {
+    const field = 'any_field'
+    const { sut } = makeSut(field)
+    const validations = sut.required().build()
+    expect(validations.length).toBe(1)
+    expect(validations[0]).toEqual(new RequiredFieldValidation(field))
+  })
+})
