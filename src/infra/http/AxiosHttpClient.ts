34369ff012ad71bc31a992919b831f9a48c4406a
Matheus Roika <matheusroika@hotmail.com>
2023-07-23 20:44:36 -0300

    fix: ensure AxiosHttpClient returns correct values on failure

--- src/infra/http/AxiosHttpClient.ts
+++ src/infra/http/AxiosHttpClient.ts
@@ -1,11 +1,17 @@
 import axios from 'axios'
+import type { AxiosError, AxiosResponse } from 'axios'
 import type { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
     const { url, body } = params
-    const httpResponse = await axios.post(url, body)
-
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.post(url, body)
+    } catch (error) {
+      const typedError = error as AxiosError
+      httpResponse = typedError.response
+    }
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
