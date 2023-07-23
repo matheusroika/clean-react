8c94b8d4ea547a045af4ef4a40cfee89795a9715
Matheus Roika <matheusroika@hotmail.com>
2023-07-23 13:59:27 -0300

    test: ensure ValidationBuilder returns MinLengthValidation

--- tests/validation/validators/validationBuilder.spec.ts
+++ tests/validation/validators/validationBuilder.spec.ts
@@ -1,5 +1,6 @@
 import { ValidationBuilder as sut } from '@/validation/validators/validationBuilder'
-import { RequiredFieldValidation } from '@/validation/validators'
+import { EmailValidation, MinLengthValidation, RequiredFieldValidation } from '@/validation/validators'
+import { mockEmailValidator } from '../mocks/mockValidation'
 
 describe('Validation Builder', () => {
   test('Should return RequiredFieldValidation', () => {
@@ -7,5 +8,21 @@ describe('Validation Builder', () => {
     const validations = sut.field(field).required().build()
     expect(validations.length).toBe(1)
     expect(validations[0]).toEqual(new RequiredFieldValidation(field))
+  })
+
+  test('Should return EmailValidation', () => {
+    const field = 'any_field'
+    const emailValidator = mockEmailValidator()
+    const validations = sut.field(field).email(emailValidator).build()
+    expect(validations.length).toBe(1)
+    expect(validations[0]).toEqual(new EmailValidation(field, emailValidator))
+  })
+
+  test('Should return MinLengthValidation', () => {
+    const field = 'any_field'
+    const minLength = 5
+    const validations = sut.field(field).minLength(minLength).build()
+    expect(validations.length).toBe(1)
+    expect(validations[0]).toEqual(new MinLengthValidation(field, minLength))
   })
 })
