57bf51eca8bbe532cbdeab0ab92366db26c790a0
Matheus Roika <matheusroika@hotmail.com>
2023-07-23 09:54:08 -0300

    feat: add EmailValidatorAdapter

---
+++ src/infra/validators/emailValidatorAdapter.ts
@@ -0,0 +1,8 @@
+import validator from 'validator'
+import type { EmailValidator } from '@/validation/protocols/emailValidator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
