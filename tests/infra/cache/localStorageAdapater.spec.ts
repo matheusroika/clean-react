e884c5f4f75f914f7510dd90f9f093dec58eaedb
Matheus Roika <matheusroika@hotmail.com>
2023-07-25 21:02:02 -0300

    test: ensure LocalStorageAdapter throws if localStorage.setItem throws

--- tests/infra/cache/localStorageAdapater.spec.ts
+++ tests/infra/cache/localStorageAdapater.spec.ts
@@ -12,11 +12,22 @@ const makeSut = (): Sut => {
 }
 
 describe('Local Storage Adapter', () => {
-  test('Should call localStorage with correct values', async () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  test('Should call localStorage.setItem with correct values', async () => {
     const { sut } = makeSut()
     const setItemSpy = jest.spyOn(Storage.prototype, 'setItem')
     const [key, value] = ['any_key', 'any_value']
     await sut.set(key, value)
     expect(setItemSpy).toHaveBeenCalledWith(key, value)
+  })
+
+  test('Should throw if localStorage.setItem throws', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(Storage.prototype, 'setItem').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.set('any_key', 'any_value')
+    await expect(promise).rejects.toThrow(new Error())
   })
 })
