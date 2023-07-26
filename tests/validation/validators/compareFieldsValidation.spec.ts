2e75a88431858dffd479daca18e1367571dd04a9
Matheus Roika <matheusroika@hotmail.com>
2023-07-26 14:15:41 -0300

    test: ensure CompareFieldsValidation returns an error if fields values are different

---
+++ tests/validation/validators/compareFieldsValidation.spec.ts
@@ -0,0 +1,27 @@
+import { CompareFieldsValidation } from '@/validation/validators/compareFieldsValidation'
+import { InvalidFieldError } from '@/validation/errors'
+
+type Sut = {
+  sut: CompareFieldsValidation
+}
+
+const makeSut = (): Sut => {
+  const sut = new CompareFieldsValidation('any_field', 'any_value')
+  return {
+    sut
+  }
+}
+
+describe('Compare Fields Validation', () => {
+  test('Should return an error if fields values are different', () => {
+    const { sut } = makeSut()
+    const error = sut.validate('')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  /* test('Should return null if field is not empty', () => {
+    const { sut } = makeSut()
+    const error = sut.validate('any_value')
+    expect(error).toBeNull()
+  }) */
+})
