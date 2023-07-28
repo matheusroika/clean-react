5c3a42499936f848bc7f9030374db2015f7760fd
Matheus Roika <matheusroika@hotmail.com>
2023-07-27 22:03:39 -0300

    test: ensure Login page loads with correct initial state

--- tests/main/cypress/e2e/login.cy.ts
+++ tests/main/cypress/e2e/login.cy.ts
@@ -1,5 +1,12 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.dataTestId('emailStatus').should('have.attr', 'title', 'Campo obrigatório').should('have.text', '🔴')
+    cy.dataTestId('passwordStatus').should('have.attr', 'title', 'Campo obrigatório').should('have.text', '🔴')
+    cy.dataTestId('submit').should('have.attr', 'disabled')
+    cy.dataTestId('modalWrapper').should('not.exist')
   })
 })
