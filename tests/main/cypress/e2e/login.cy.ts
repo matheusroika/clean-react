37c5930e44abbd25d323ab75269cc73f478bff2a
Matheus Roika <matheusroika@hotmail.com>
2023-07-27 22:08:59 -0300

    test: ensure Login page presents error message if form is invalid

--- tests/main/cypress/e2e/login.cy.ts
+++ tests/main/cypress/e2e/login.cy.ts
@@ -9,4 +9,13 @@ describe('Login', () => {
     cy.dataTestId('submit').should('have.attr', 'disabled')
     cy.dataTestId('modalWrapper').should('not.exist')
   })
+
+  it('Should present error message if form is invalid', () => {
+    cy.dataTestId('email').type('invalid_email')
+    cy.dataTestId('emailStatus').should('have.attr', 'title', 'Campo inválido').should('have.text', '🔴')
+    cy.dataTestId('password').type('123')
+    cy.dataTestId('passwordStatus').should('have.attr', 'title', 'Campo requer 5 caracteres no mínimo').should('have.text', '🔴')
+    cy.dataTestId('submit').should('have.attr', 'disabled')
+    cy.dataTestId('modalWrapper').should('not.exist')
+  })
 })
