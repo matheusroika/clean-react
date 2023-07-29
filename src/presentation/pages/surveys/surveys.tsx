ff54975a51af340a9962d9da45e702c75ab2e19d
Matheus Roika <matheusroika@hotmail.com>
2023-07-29 20:21:47 -0300

    feat: add answered icon to Surveys page to show which surveys user already answered

--- src/presentation/pages/surveys/surveys.tsx
+++ src/presentation/pages/surveys/surveys.tsx
@@ -20,6 +20,9 @@ const Surveys: React.FC = () => {
         <ul>
           <li>
             <div>
+              <div className={[styles.icon, styles.notAnswered].join(' ')}>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAARCAQAAAB3TUQ1AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfnBx0RDghaer4zAAAAnklEQVQoz43QsQkCQRSE4X8XrcHc0OxasBAjMRO5DmxDrEN7ODCyBgMzI0HQec/gBO/gVt9EC+9jYBYAH2mvu/eilxqbAejg7g6ALX0wagDaNwA6F6B60OZezBdm8A2R+FQKNdo650hh0jVPfpwTtL3pM/wvzHYJTSGnVZnaDcCOAyftuputLn/WqeO2ZTbWI8DAqhADW4QYWKVnib0B2pb1YBTGpj8AAAAldEVYdGRhdGU6Y3JlYXRlADIwMjMtMDctMjlUMjA6MTQ6MDgtMDM6MDColWdjAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIzLTA3LTI5VDIwOjE0OjA4LTAzOjAw2cjf3wAAAABJRU5ErkJggg==" />
+              </div>
               <time>
                 <span className={styles.day}>22</span>
                 <span className={styles.month}>07</span>
