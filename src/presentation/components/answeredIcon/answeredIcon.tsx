315dd49e5d29eec1015892702900f595d9b3218b
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 15:12:16 -0300

    feat: ensure SurveyItem component renders with correct values

--- src/presentation/components/answeredIcon/answeredIcon.tsx
+++ src/presentation/components/answeredIcon/answeredIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './answeredIconStyles.scss'
 
-const IconName = {
+export const IconName = {
   thumbsUp: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAARCAQAAAB3TUQ1AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfnBx0RDhkwyp7BAAAAnklEQVQoz42RsQkCQRBF3x7aioGRV4ORTdiDHVwNZgfmNqBFiIamJhqZmAnCMfMNDkWWW5kXLczjD/MXBlAjWacZ/1EjSZIvQ5rkdUizl8YBTbJjUfOVfrA2T5FkOwB7qIBdfZEkgJSgfxW23YIiVITwe1BkG1rtniaxxH11oS/gU9QwPv+OAcwKLZ7zq0+FYtbZz/jUDnmqPW2jEcAbWGHY3H/kdsoAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjMtMDctMjlUMjA6MTQ6MjUtMDM6MDCLZwHeAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIzLTA3LTI5VDIwOjE0OjI1LTAzOjAw+jq5YgAAAABJRU5ErkJggg==',
   thumbsDown: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAARCAQAAAB3TUQ1AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfnBx0RDghaer4zAAAAnklEQVQoz43QsQkCQRSE4X8XrcHc0OxasBAjMRO5DmxDrEN7ODCyBgMzI0HQec/gBO/gVt9EC+9jYBYAH2mvu/eilxqbAejg7g6ALX0wagDaNwA6F6B60OZezBdm8A2R+FQKNdo650hh0jVPfpwTtL3pM/wvzHYJTSGnVZnaDcCOAyftuputLn/WqeO2ZTbWI8DAqhADW4QYWKVnib0B2pb1YBTGpj8AAAAldEVYdGRhdGU6Y3JlYXRlADIwMjMtMDctMjlUMjA6MTQ6MDgtMDM6MDColWdjAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIzLTA3LTI5VDIwOjE0OjA4LTAzOjAw2cjf3wAAAABJRU5ErkJggg=='
 }
@@ -16,7 +16,7 @@ const AnsweredIcon: React.FC<Props> = ({ iconName, className }) => {
 
   return (
     <div className={[styles.icon, iconColorStyle, className].join(' ')}>
-      <img src={IconName[iconName]} />
+      <img data-testid="iconImg" src={IconName[iconName]} />
     </div>
   )
 }
--- src/presentation/pages/surveys/components/surveyItem/surveyItem.tsx
+++ src/presentation/pages/surveys/components/surveyItem/surveyItem.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import styles from './surveyItemStyles.scss'
 import AnsweredIcon from '@/presentation/components/answeredIcon/answeredIcon'
+import { type Survey } from '@/domain/models/Survey'
 
-const SurveyItem: React.FC = () => {
+type Props = {
+  survey: Survey
+}
+
+const SurveyItem: React.FC<Props> = ({ survey }) => {
   return (
     <li>
       <div>
         <AnsweredIcon iconName='thumbsDown' className={styles.icon} />
         <time>
-          <span className={styles.day}>22</span>
-          <span className={styles.month}>JUL</span>
-          <span className={styles.year}>2023</span>
+          <span data-testid='day' className={styles.day}>03</span>
+          <span data-testid='month' className={styles.month}>jul</span>
+          <span data-testid='year' className={styles.year}>2023</span>
         </time>
-        <p>Qual é seu framework web favorito?</p>
+        <p data-testid='question'>{survey.question}</p>
       </div>
-      <footer>Ver resultado</footer>
+      <footer data-testid='result'>Ver resultado</footer>
     </li>
   )
 }
