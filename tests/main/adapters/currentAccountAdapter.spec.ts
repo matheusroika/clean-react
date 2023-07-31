df2a47fdb764829680f6c265a3853bf585ca13b2
Matheus Roika <matheusroika@hotmail.com>
2023-07-30 21:16:01 -0300

    test: ensure CurrentAccountAdapter throws UnexpectedError if any param is invalid

--- tests/main/adapters/currentAccountAdapter.spec.ts
+++ tests/main/adapters/currentAccountAdapter.spec.ts
@@ -1,6 +1,7 @@
 import { setCurrentAccountAdapter } from '@/main/adapters/currentAccountAdapter'
 import { LocalStorageAdapter } from '@/infra/cache/localStorageAdapter'
 import { mockAccount } from '../../domain/mocks'
+import { UnexpectedError } from '@/domain/errors'
 
 jest.mock('@/infra/cache/localStorageAdapter')
 
@@ -10,5 +11,12 @@ describe('Current Account Adapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should throw UnexpectedError if param is invalid', () => {
+    expect(() => { setCurrentAccountAdapter(undefined) }).toThrow(new UnexpectedError())
+    expect(() => { setCurrentAccountAdapter({ name: undefined, email: 'any@email.com', accessToken: 'any_token' }) }).toThrow(new UnexpectedError())
+    expect(() => { setCurrentAccountAdapter({ name: 'Any Name', email: undefined, accessToken: 'any_token' }) }).toThrow(new UnexpectedError())
+    expect(() => { setCurrentAccountAdapter({ name: 'Any Name', email: 'any@email.com', accessToken: undefined }) }).toThrow(new UnexpectedError())
   })
 })
