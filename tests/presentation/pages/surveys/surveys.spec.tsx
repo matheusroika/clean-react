217738a0fc43f54b2f36a20d87060ad5d971e138
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 14:18:14 -0300

    test: ensure Surveys page loads a Survey item correctly

--- tests/presentation/pages/surveys/surveys.spec.tsx
+++ tests/presentation/pages/surveys/surveys.spec.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { type RenderResult, render } from '@testing-library/react'
 import Surveys from '@/presentation/pages/surveys/surveys'
+import { type LoadSurveys } from '@/domain/useCases/LoadSurveys'
+import { type Survey } from '@/domain/models/Survey'
+import { mockSurvey } from '@/../tests/domain/mocks'
 
 type Sut = {
   sut: RenderResult
+  loadSurveysStub: LoadSurveys
+}
+
+const mockLoadSurveys = (): LoadSurveys => {
+  class LoadSurveyStub implements LoadSurveys {
+    async loadAll (): Promise<Survey[]> {
+      return [mockSurvey()]
+    }
+  }
+  return new LoadSurveyStub()
 }
 
 const makeSut = (): Sut => {
-  const sut = render(<Surveys />)
+  const loadSurveysStub = mockLoadSurveys()
+  const sut = render(<Surveys loadSurveys={loadSurveysStub} />)
   return {
-    sut
+    sut,
+    loadSurveysStub
   }
 }
 
@@ -19,5 +34,11 @@ describe('Surveys Page', () => {
     const surveyList = sut.getByTestId('surveyList')
     expect(surveyList.querySelectorAll('li').length).toBe(4)
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('Should load a Survey item correctly', async () => {
+    const { sut } = makeSut()
+    const surveyItem = await sut.findByText('Qual é seu framework web favorito?')
+    expect(surveyItem).toBeTruthy()
   })
 })
