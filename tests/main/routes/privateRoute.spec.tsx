d34e59cfe2e54cadaa9e1923a89c4d87a7f35321
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 00:53:44 -0300

    test: ensure privateRoute redirects to /login if account is empty

--- tests/main/routes/privateRoute.spec.tsx
+++ tests/main/routes/privateRoute.spec.tsx
@@ -1,24 +1,46 @@
 import React from 'react'
-import { type RenderResult, render, waitFor } from '@testing-library/react'
+import { type RenderResult, render, waitFor, cleanup } from '@testing-library/react'
 import PrivateRoute from '@/main/routes/privateRoute'
 import MockPage from '../../presentation/mocks/mockPage'
+import ApiContext from '@/presentation/contexts/apiContext'
+import { mockAccount } from '../../domain/mocks'
+import { Routes, Route, MemoryRouter } from 'react-router-dom'
+import { type Account } from '@/domain/models/Account'
 
 type Sut = {
   sut: RenderResult
 }
 
-const makeSut = (): Sut => {
-  const sut = render(<PrivateRoute Page={MockPage} />)
+const makeSut = (account: Account): Sut => {
+  const sut = render(
+    <ApiContext.Provider value={{ getCurrentAccount: () => account, setCurrentAccount: jest.fn() }}>
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path='/' element={<PrivateRoute Page={MockPage} />} />
+          <Route path='/login' element={<h1>Test Login Page</h1>} />
+        </Routes>
+      </MemoryRouter>
+    </ApiContext.Provider>
+  )
   return {
     sut
   }
 }
 
 describe('Private Route', () => {
-  test('Should render param Page correctly', async () => {
-    const { sut } = makeSut()
+  afterEach(cleanup)
+
+  test('Should render passed Page correctly', async () => {
+    const { sut } = makeSut(mockAccount())
     await waitFor(() => {
       expect(sut.getByText('Test Page')).toBeTruthy()
+    })
+  })
+
+  test('Should redirect to /login if account is empty', async () => {
+    const { sut } = makeSut(undefined)
+    await waitFor(() => {
+      expect(sut.getByText('Test Login Page')).toBeTruthy()
     })
   })
 })
