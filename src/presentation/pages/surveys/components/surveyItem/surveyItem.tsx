5371d88c4e7ecb70323a3b78cabdf2d46f18e0e7
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 15:32:13 -0300

    feat: ensure SurveyItem component renders with other correct values

--- src/presentation/pages/surveys/components/surveyItem/surveyItem.tsx
+++ src/presentation/pages/surveys/components/surveyItem/surveyItem.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 import styles from './surveyItemStyles.scss'
 import AnsweredIcon from '@/presentation/components/answeredIcon/answeredIcon'
 import { type Survey } from '@/domain/models/Survey'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 type Props = {
   survey: Survey
 }
 
 const SurveyItem: React.FC<Props> = ({ survey }) => {
+  const [formattedDay, formattedMonth, formattedYear] = format(survey.date, 'dd-LLL-y', { locale: ptBR }).split('-')
   return (
     <li>
       <div>
-        <AnsweredIcon iconName='thumbsDown' className={styles.icon} />
+        <AnsweredIcon iconName={survey.answered ? 'thumbsUp' : 'thumbsDown'} className={styles.icon} />
         <time>
-          <span data-testid='day' className={styles.day}>03</span>
-          <span data-testid='month' className={styles.month}>jul</span>
-          <span data-testid='year' className={styles.year}>2023</span>
+          <span data-testid='day' className={styles.day}>{formattedDay}</span>
+          <span data-testid='month' className={styles.month}>{formattedMonth}</span>
+          <span data-testid='year' className={styles.year}>{formattedYear}</span>
         </time>
         <p data-testid='question'>{survey.question}</p>
       </div>
