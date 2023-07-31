0d831c29e0b216b371a44d627c41176d44227c5e
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 00:53:19 -0300

    feat: ensure privateRoute redirects to /login if account is empty

--- src/main/routes/privateRoute.tsx
+++ src/main/routes/privateRoute.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import apiContext from '@/presentation/contexts/apiContext'
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
 
 type Props = {
   Page: React.FC
 }
 
 const PrivateRoute: React.FC<Props> = ({ Page }) => {
-  return <Page />
+  const { getCurrentAccount } = useContext(apiContext)
+  const account = getCurrentAccount()
+  const isAccountInvalid = !account?.accessToken || !account?.name || !account?.email
+  console.log(`ACCOUNT: ${JSON.stringify(account)}, ${isAccountInvalid.toString()}`)
+  return isAccountInvalid ? <Navigate to='/login' /> : <Page />
 }
 
 export default PrivateRoute
