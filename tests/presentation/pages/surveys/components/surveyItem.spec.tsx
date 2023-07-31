31ded5a47fd43fddac2225522f76bf2923ea8d55
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 15:32:57 -0300

    test: ensure SurveyItem component renders with other correct values

--- tests/presentation/pages/surveys/components/surveyItem.spec.tsx
+++ tests/presentation/pages/surveys/components/surveyItem.spec.tsx
@@ -3,13 +3,14 @@ import { type RenderResult, render } from '@testing-library/react'
 import SurveyItem from '@/presentation/pages/surveys/components/surveyItem/surveyItem'
 import { mockSurvey } from '@/../tests/domain/mocks'
 import { IconName } from '@/presentation/components/answeredIcon/answeredIcon'
+import { type Survey } from '@/domain/models/Survey'
 
 type Sut = {
   sut: RenderResult
 }
 
-const makeSut = (): Sut => {
-  const sut = render(<SurveyItem survey={mockSurvey()} />)
+const makeSut = (survey: Survey): Sut => {
+  const sut = render(<SurveyItem survey={survey} />)
   return {
     sut
   }
@@ -17,14 +18,29 @@ const makeSut = (): Sut => {
 
 describe('Survey Item Component', () => {
   test('Should render with correct values', () => {
-    const { sut } = makeSut()
     const survey = mockSurvey()
+    const { sut } = makeSut(survey)
     const iconImg = sut.getByTestId('iconImg') as HTMLImageElement
     expect(iconImg.src).toBe(IconName.thumbsDown)
     expect(sut.getByTestId('question').textContent).toBe(survey.question)
     expect(sut.getByTestId('day').textContent).toBe('03')
     expect(sut.getByTestId('month').textContent).toBe('jul')
     expect(sut.getByTestId('year').textContent).toBe('2023')
+    expect(sut.getByTestId('result').textContent).toBe('Ver resultado')
+  })
+
+  test('Should render with other correct values', () => {
+    const survey = mockSurvey()
+    survey.question = 'other_question'
+    survey.answered = true
+    survey.date = new Date('2022-02-28T05:52:28.514Z')
+    const { sut } = makeSut(survey)
+    const iconImg = sut.getByTestId('iconImg') as HTMLImageElement
+    expect(iconImg.src).toBe(IconName.thumbsUp)
+    expect(sut.getByTestId('question').textContent).toBe(survey.question)
+    expect(sut.getByTestId('day').textContent).toBe('28')
+    expect(sut.getByTestId('month').textContent).toBe('fev')
+    expect(sut.getByTestId('year').textContent).toBe('2022')
     expect(sut.getByTestId('result').textContent).toBe('Ver resultado')
   })
 })
