ed0c9a4cf7323339010b477189c30ae0da4de8d9
Matheus Roika <matheusroika@hotmail.com>
2023-08-01 00:01:28 -0300

    refactor: remove account check from currentAccountAdapter

--- src/main/adapters/currentAccountAdapter.ts
+++ src/main/adapters/currentAccountAdapter.ts
@@ -1,9 +1,7 @@
 import { makeLocalStorageAdapter } from '../factories/cache/localStorageAdapterFactory'
-import { UnexpectedError } from '@/domain/errors'
 import type { Account } from '@/domain/models/Account'
 
 export const setCurrentAccountAdapter = (account: Account): void => {
-  if (!account?.accessToken || !account?.name || !account?.email) throw new UnexpectedError()
   makeLocalStorageAdapter().set('account', account)
 }
 
--- tests/main/adapters/currentAccountAdapter.spec.ts
+++ tests/main/adapters/currentAccountAdapter.spec.ts
@@ -1,7 +1,6 @@
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters/currentAccountAdapter'
 import { LocalStorageAdapter } from '@/infra/cache/localStorageAdapter'
 import { mockAccount } from '../../domain/mocks'
-import { UnexpectedError } from '@/domain/errors'
 
 jest.mock('@/infra/cache/localStorageAdapter')
 
@@ -11,13 +10,6 @@ describe('Current Account Adapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexpectedError if localStorageAdapter.set receives invalid param', () => {
-    expect(() => { setCurrentAccountAdapter(undefined) }).toThrow(new UnexpectedError())
-    expect(() => { setCurrentAccountAdapter({ name: undefined, email: 'any@email.com', accessToken: 'any_token' }) }).toThrow(new UnexpectedError())
-    expect(() => { setCurrentAccountAdapter({ name: 'Any Name', email: undefined, accessToken: 'any_token' }) }).toThrow(new UnexpectedError())
-    expect(() => { setCurrentAccountAdapter({ name: 'Any Name', email: 'any@email.com', accessToken: undefined }) }).toThrow(new UnexpectedError())
   })
 
   test('Should call localStorageAdapter.get with correct values', () => {
