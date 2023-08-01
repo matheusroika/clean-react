2a24aadf3372667fed8d22bd4d98da0bc64f32d6
Matheus Roika <matheusroika@hotmail.com>
2023-08-01 00:49:18 -0300

    feat: ensure RemoteLoadSurveys throws AccessDeniedError and redirect to /login if HttpGetClient returns 403

--- src/data/useCases/loadSurveys/remoteLoadSurveys.ts
+++ src/data/useCases/loadSurveys/remoteLoadSurveys.ts
@@ -1,5 +1,5 @@
 import { HttpStatusCode } from '@/data/protocols/http'
-import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import type { LoadSurveys } from '@/domain/useCases/LoadSurveys'
 import type { Survey } from '@/domain/models/Survey'
 import type { HttpGetClient } from '@/data/protocols/http'
@@ -16,10 +16,8 @@ export class RemoteLoadSurveys implements LoadSurveys {
 
   async loadAll (): Promise<Survey[]> {
     const account: Account = this.getStorage.get('account')
-    if (!account?.accessToken || !account?.name || !account?.email) {
-      await this.redirect('/login')
-      throw new InvalidCredentialsError()
-    }
+    if (!account?.accessToken || !account?.name || !account?.email) await this.accessDenied()
+
     const httpResponse = await this.httpGetClient.get({
       url: this.url,
       headers: {
@@ -30,7 +28,15 @@ export class RemoteLoadSurveys implements LoadSurveys {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.noContent: return []
+      case HttpStatusCode.forbidden:
+        await this.accessDenied()
+        break
       default: throw new UnexpectedError()
     }
+  }
+
+  private async accessDenied (): Promise<void> {
+    await this.redirect('/login')
+    throw new AccessDeniedError()
   }
 }
