645dccddda308dde355170e44d63c4060cea5598
Matheus Roika <matheusroika@hotmail.com>
2023-08-01 00:23:09 -0300

    feat: ensure Header component calls setCurrentAccount with null and redirects to /login on logout click

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import apiContext from '@/presentation/contexts/apiContext'
 import styles from './headerStyles.scss'
 import Logo from '../logo/logo'
 
 const Header: React.FC = () => {
+  const { setCurrentAccount } = useContext(apiContext)
+  const navigate = useNavigate()
+
+  const logout = (): void => {
+    setCurrentAccount(null)
+    navigate('/login')
+  }
+
   return (
     <header>
       <div>
         <Logo />
         <div className={styles.profile}>
           <span>Nome do usuário</span>
-          <button>Sair</button>
+          <button data-testid='logout' onClick={logout}>Sair</button>
         </div>
       </div>
     </header>
