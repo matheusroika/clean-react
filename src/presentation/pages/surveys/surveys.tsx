99adfae851a7d9e590cdbb35bcb3cf54ac0ea277
Matheus Roika <matheusroika@hotmail.com>
2023-08-01 01:57:53 -0300

    refactor: apply useErrorHandler in Surveys page

--- src/presentation/pages/surveys/surveys.tsx
+++ src/presentation/pages/surveys/surveys.tsx
@@ -1,22 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import apiContext from '@/presentation/contexts/apiContext'
+import React, { useEffect, useState } from 'react'
 import styles from './surveysStyles.scss'
 import Header from '@/presentation/components/header/header'
 import Footer from '@/presentation/components/footer/footer'
 import SurveyList from './components/surveyList/surveyList'
 import SurveysError from './components/surveysError/surveysError'
-import { AccessDeniedError } from '@/domain/errors'
 import { type LoadSurveys } from '@/domain/useCases/LoadSurveys'
 import { type Survey } from '@/domain/models/Survey'
+import { useErrorHandler } from '@/presentation/hooks/useErrorHandler'
 
 type Props = {
   loadSurveys: LoadSurveys
 }
 
 const Surveys: React.FC<Props> = ({ loadSurveys }) => {
-  const { setCurrentAccount } = useContext(apiContext)
-  const navigate = useNavigate()
+  const handleError = useErrorHandler((error: Error) => { setError(error.message) })
   const [surveys, setSurveys] = useState<Survey[]>(null)
   const [error, setError] = useState('')
 
@@ -26,13 +23,8 @@ const Surveys: React.FC<Props> = ({ loadSurveys }) => {
       setSurveys(loadedSurveys)
       setError('')
     } catch (error) {
-      if (error instanceof AccessDeniedError) {
-        setCurrentAccount(null)
-        navigate('/login')
-      } else {
-        const typedError = error as Error
-        setError(typedError.message)
-      }
+      const typedError = error as Error
+      handleError(typedError)
     }
   }
 
