a6b34d599095ef82575e605bff97b7b1a435d7e1
Matheus Roika <matheusroika@hotmail.com>
2023-07-31 21:54:00 -0300

    feat: add headers to remoteLoadSurveys httpGetClient request

--- src/data/useCases/loadSurveys/remoteLoadSurveys.ts
+++ src/data/useCases/loadSurveys/remoteLoadSurveys.ts
@@ -3,16 +3,24 @@ import { UnexpectedError } from '@/domain/errors'
 import type { LoadSurveys } from '@/domain/useCases/LoadSurveys'
 import type { Survey } from '@/domain/models/Survey'
 import type { HttpGetClient } from '@/data/protocols/http'
+import type { GetStorage } from '@/data/protocols/cache'
+import type { Account } from '@/domain/models/Account'
 
 export class RemoteLoadSurveys implements LoadSurveys {
   constructor (
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient<any, Survey[]>
+    private readonly httpGetClient: HttpGetClient<any, Survey[]>,
+    private readonly getStorage: GetStorage
   ) {}
 
   async loadAll (): Promise<Survey[]> {
+    const account: Account = this.getStorage.get('account')
+    if (!account?.accessToken || !account?.name || !account?.email) throw new UnexpectedError()
     const httpResponse = await this.httpGetClient.get({
-      url: this.url
+      url: this.url,
+      headers: {
+        'x-access-token': account.accessToken
+      }
     })
 
     switch (httpResponse.statusCode) {
--- src/main/factories/useCases/loadSurveys/remoteLoadSurveysFactory.ts
+++ src/main/factories/useCases/loadSurveys/remoteLoadSurveysFactory.ts
@@ -1,7 +1,8 @@
 import { RemoteLoadSurveys } from '@/data/useCases/loadSurveys/remoteLoadSurveys'
 import { makeApiUrl } from '../../http/apiUrlFactory'
 import { makeAxiosHttpClient } from '../../http/axiosHttpClientFactory'
+import { makeLocalStorageAdapter } from '../../cache/localStorageAdapterFactory'
 
 export const makeRemoteLoadSurveys = (): RemoteLoadSurveys => {
-  return new RemoteLoadSurveys(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveys(makeApiUrl('/surveys'), makeAxiosHttpClient(), makeLocalStorageAdapter())
 }
