a4725990a8d21246a80c17ed7ad5a9583e46f228
Matheus Roika <matheusroika@hotmail.com>
2023-08-02 16:36:25 -0300

    test: ensure Surveys page shows surveyItems correctly

--- tests/domain/mocks/mockSurvey.ts
+++ tests/domain/mocks/mockSurvey.ts
@@ -7,6 +7,13 @@ export const mockAnswer = (): Answer => ({
   isCurrentAccountAnswer: false
 })
 
+export const mockOtherAnswer = (): Answer => ({
+  answer: 'other_answer',
+  count: 5,
+  percent: 100,
+  isCurrentAccountAnswer: true
+})
+
 export const mockSurvey = (): Survey => ({
   id: 'any_id',
   question: 'any_question',
@@ -16,10 +23,20 @@ export const mockSurvey = (): Survey => ({
   answered: false
 })
 
+export const mockOtherSurvey = (): Survey => ({
+  id: 'other_id',
+  question: 'other_question',
+  answers: [mockOtherAnswer()],
+  totalResponses: 5,
+  date: new Date('2022-12-25T05:52:28.514Z'),
+  answered: true
+})
+
 export const mockSurveys = (amount = 1): Survey[] => {
   const surveys: Survey[] = []
   for (let i = 0; i < amount; i++) {
-    surveys.push(mockSurvey())
+    if (i % 2 === 0) surveys.push(mockSurvey())
+    else surveys.push(mockOtherSurvey())
   }
   return surveys
 }
--- tests/main/cypress/e2e/surveys.cy.ts
+++ tests/main/cypress/e2e/surveys.cy.ts
@@ -1,11 +1,17 @@
 import * as httpMocks from '../utils/httpMocks'
-import { mockAccount } from '../../../domain/mocks'
+import { mockAccount, mockSurveys } from '../../../domain/mocks'
 
 const baseUrl: string = Cypress.config().baseUrl
 
 const http = {
   mockUnexpectedError: (): void => { httpMocks.mockServerError(/surveys/, 'GET', 'surveys') },
-  mockAccessDeniedError: (): void => { httpMocks.mockForbiddenError(/surveys/, 'GET', 'surveys') }
+  mockAccessDeniedError: (): void => { httpMocks.mockForbiddenError(/surveys/, 'GET', 'surveys') },
+  mockOk: (): void => { httpMocks.mockOk(/surveys/, 'GET', mockSurveys(2), 'surveys') }
+}
+
+const iconName = {
+  thumbsUp: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAARCAQAAAB3TUQ1AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfnBx0RDhkwyp7BAAAAnklEQVQoz42RsQkCQRBF3x7aioGRV4ORTdiDHVwNZgfmNqBFiIamJhqZmAnCMfMNDkWWW5kXLczjD/MXBlAjWacZ/1EjSZIvQ5rkdUizl8YBTbJjUfOVfrA2T5FkOwB7qIBdfZEkgJSgfxW23YIiVITwe1BkG1rtniaxxH11oS/gU9QwPv+OAcwKLZ7zq0+FYtbZz/jUDnmqPW2jEcAbWGHY3H/kdsoAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjMtMDctMjlUMjA6MTQ6MjUtMDM6MDCLZwHeAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIzLTA3LTI5VDIwOjE0OjI1LTAzOjAw+jq5YgAAAABJRU5ErkJggg==',
+  thumbsDown: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAARCAQAAAB3TUQ1AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfnBx0RDghaer4zAAAAnklEQVQoz43QsQkCQRSE4X8XrcHc0OxasBAjMRO5DmxDrEN7ODCyBgMzI0HQec/gBO/gVt9EC+9jYBYAH2mvu/eilxqbAejg7g6ALX0wagDaNwA6F6B60OZezBdm8A2R+FQKNdo650hh0jVPfpwTtL3pM/wvzHYJTSGnVZnaDcCOAyftuputLn/WqeO2ZTbWI8DAqhADW4QYWKVnib0B2pb1YBTGpj8AAAAldEVYdGRhdGU6Y3JlYXRlADIwMjMtMDctMjlUMjA6MTQ6MDgtMDM6MDColWdjAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIzLTA3LTI5VDIwOjE0OjA4LTAzOjAw2cjf3wAAAABJRU5ErkJggg=='
 }
 
 describe('Surveys', () => {
@@ -18,6 +24,26 @@ describe('Surveys', () => {
     })
     localStorage.setItem('account', JSON.stringify(mockAccount()))
     cy.visit('')
+  })
+
+  it('Should show surveyItems correctly', () => {
+    http.mockOk()
+    cy.get('li:empty').should('have.length', 4)
+    cy.get('li:not(:empty)').should('have.length', 2)
+    cy.get('li:first-child').then(li => {
+      assert.equal(li.find('[data-testid="day"]').text(), '03')
+      assert.equal(li.find('[data-testid="month"]').text(), 'jul')
+      assert.equal(li.find('[data-testid="year"]').text(), '2023')
+      assert.equal(li.find('[data-testid="question"]').text(), 'any_question')
+      assert.equal(li.find('[data-testid="iconImg"]').attr('src'), iconName.thumbsDown)
+    })
+    cy.get('li:last-child').then(li => {
+      assert.equal(li.find('[data-testid="day"]').text(), '25')
+      assert.equal(li.find('[data-testid="month"]').text(), 'dez')
+      assert.equal(li.find('[data-testid="year"]').text(), '2022')
+      assert.equal(li.find('[data-testid="question"]').text(), 'other_question')
+      assert.equal(li.find('[data-testid="iconImg"]').attr('src'), iconName.thumbsUp)
+    })
   })
 
   it('Should show correct username on header', () => {
