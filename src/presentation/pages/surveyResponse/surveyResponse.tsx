335bf7f0019228e9f6e5f36f81cc72ab86316306
Matheus Roika <matheusroika@hotmail.com>
2023-08-04 03:41:21 -0300

    feat: add loading shimmer on SurveyResponse page

--- src/presentation/pages/surveyResponse/surveyResponse.tsx
+++ src/presentation/pages/surveyResponse/surveyResponse.tsx
@@ -11,6 +11,7 @@ const SurveyResponse: React.FC = () => {
     <div className={styles.surveyResponse}>
       <Header />
       <main>
+        {false && <>
         <hgroup>
           <Calendar date={new Date()} className={styles.calendar}/>
           <h1>Qual sua plataforma de cursos preferida?</h1>
@@ -21,21 +22,10 @@ const SurveyResponse: React.FC = () => {
             <span className={styles.answer}>Amazon Web Services (AWS)</span>
             <span className={styles.percent}>50%</span>
           </li>
-
-          <li className={styles.userAnswer}>
-            <img src="https://clean-node-api-rlnz.onrender.com/static/img/aws.webp" alt="Amazon Web Services (AWS)" />
-            <span className={styles.answer}>Amazon Web Services (AWS)</span>
-            <span className={styles.percent}>50%</span>
-          </li>
-
-          <li>
-            <img src="https://clean-node-api-rlnz.onrender.com/static/img/aws.webp" alt="Amazon Web Services (AWS)" />
-            <span className={styles.answer}>Amazon Web Services (AWS)</span>
-            <span className={styles.percent}>50%</span>
-          </li>
         </FlipMove>
         <button>Voltar</button>
         {false && <Loading />}
+        </>}
       </main>
       <Footer />
     </div>
