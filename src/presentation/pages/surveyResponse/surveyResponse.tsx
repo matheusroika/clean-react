fa7268b117f6f6c7d8840d191f111f4503148504
Matheus Roika <matheusroika@hotmail.com>
2023-08-04 03:42:49 -0300

    refactor: change highlight from border to box-shadow in SurveyResponse page

--- src/presentation/pages/surveyResponse/surveyResponse.tsx
+++ src/presentation/pages/surveyResponse/surveyResponse.tsx
@@ -11,12 +11,24 @@ const SurveyResponse: React.FC = () => {
     <div className={styles.surveyResponse}>
       <Header />
       <main>
-        {false && <>
+        {true && <>
         <hgroup>
           <Calendar date={new Date()} className={styles.calendar}/>
           <h1>Qual sua plataforma de cursos preferida?</h1>
         </hgroup>
         <FlipMove className={styles.answers}>
+          <li>
+            <img src="https://clean-node-api-rlnz.onrender.com/static/img/aws.webp" alt="Amazon Web Services (AWS)" />
+            <span className={styles.answer}>Amazon Web Services (AWS)</span>
+            <span className={styles.percent}>50%</span>
+          </li>
+
+          <li className={styles.userAnswer}>
+            <img src="https://clean-node-api-rlnz.onrender.com/static/img/aws.webp" alt="Amazon Web Services (AWS)" />
+            <span className={styles.answer}>Amazon Web Services (AWS)</span>
+            <span className={styles.percent}>50%</span>
+          </li>
+
           <li>
             <img src="https://clean-node-api-rlnz.onrender.com/static/img/aws.webp" alt="Amazon Web Services (AWS)" />
             <span className={styles.answer}>Amazon Web Services (AWS)</span>
