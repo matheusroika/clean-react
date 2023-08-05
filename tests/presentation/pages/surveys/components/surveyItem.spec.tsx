f3e7f183ceccc587f3f7131d3b4bb17ce59d6bf5
Matheus Roika <matheusroika@hotmail.com>
2023-08-05 00:56:00 -0300

    test: ensure SurveyItem component goes to SurveyResponse page on click

--- tests/presentation/pages/surveys/components/surveyItem.spec.tsx
+++ tests/presentation/pages/surveys/components/surveyItem.spec.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import { type RenderResult, render } from '@testing-library/react'
+import { type RenderResult, render, fireEvent } from '@testing-library/react'
 import SurveyItem from '@/presentation/pages/surveys/components/surveyItem/surveyItem'
 import { mockOtherSurvey, mockSurvey } from '@/../tests/domain/mocks'
 import { IconName } from '@/presentation/pages/surveys/components/answeredIcon/answeredIcon'
 import { type Survey } from '@/domain/models/Survey'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 
 type Sut = {
   sut: RenderResult
 }
 
 const makeSut = (survey: Survey): Sut => {
-  const sut = render(<SurveyItem survey={survey} />)
+  const sut = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Routes>
+        <Route path='/' element={<SurveyItem survey={survey} />}/>
+        <Route path='/surveys/any_id' element={<h1>Test Pass Survey Response</h1>}/>
+      </Routes>
+    </MemoryRouter>
+  )
   return {
     sut
   }
@@ -39,5 +47,13 @@ describe('Survey Item Component', () => {
     expect(sut.getByTestId('month').textContent).toBe('dez')
     expect(sut.getByTestId('year').textContent).toBe('2022')
     expect(sut.getByTestId('result').textContent).toBe('Ver resultado')
+  })
+
+  test('Should go to SurveyResponse page on click', async () => {
+    const survey = mockSurvey()
+    const { sut } = makeSut(survey)
+    fireEvent.click(sut.getByTestId('result'))
+    const surveyResponse = await sut.findByText('Test Pass Survey Response')
+    expect(surveyResponse).toBeTruthy()
   })
 })
