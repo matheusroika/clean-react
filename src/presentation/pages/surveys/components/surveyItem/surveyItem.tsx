4735818095c49009e22dbace47edd1c43e0cb409
Matheus Roika <matheusroika@hotmail.com>
2023-08-05 00:55:30 -0300

    feat: ensure SurveyItem component goes to SurveyResponse page on click

--- src/presentation/pages/surveys/components/surveyItem/surveyItem.tsx
+++ src/presentation/pages/surveys/components/surveyItem/surveyItem.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './surveyItemStyles.scss'
 import AnsweredIcon from '@/presentation/pages/surveys/components/answeredIcon/answeredIcon'
-import { type Survey } from '@/domain/models/Survey'
 import Calendar from '@/presentation/components/calendar/calendar'
+import type { Survey } from '@/domain/models/Survey'
 
 type Props = {
   survey: Survey
 }
 
 const SurveyItem: React.FC<Props> = ({ survey }) => {
+  const navigate = useNavigate()
   return (
     <li className={styles.surveyItem}>
       <div>
@@ -16,7 +18,7 @@ const SurveyItem: React.FC<Props> = ({ survey }) => {
         <Calendar date={new Date(survey.date)} className={styles.calendar} />
         <p data-testid='question'>{survey.question}</p>
       </div>
-      <footer data-testid='result'>Ver resultado</footer>
+      <footer data-testid='result' onClick={() => { navigate(`/surveys/${survey.id}`) }}>Ver resultado</footer>
     </li>
   )
 }
