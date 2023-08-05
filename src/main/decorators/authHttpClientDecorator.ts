1ede67637745ee70f261f3f362ea657127d38483
Matheus Roika <matheusroika@hotmail.com>
2023-08-05 16:11:02 -0300

    feat: ensure AuthHttpClientDecorator adds auth headers if GetStorage returns a valid value

--- src/main/decorators/authHttpClientDecorator.ts
+++ src/main/decorators/authHttpClientDecorator.ts
@@ -1,5 +1,6 @@
 import type { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols/http'
 import type { GetStorage } from '@/data/protocols/cache'
+import type { Account } from '@/domain/models/Account'
 
 export class AuthHttpClientDecorator implements HttpClient<any, any, any> {
   constructor (
@@ -8,8 +9,18 @@ export class AuthHttpClientDecorator implements HttpClient<any, any, any> {
   ) {}
 
   async request (data: HttpRequest<any, any>): Promise<HttpResponse<any>> {
-    this.getStorage.get('account')
-    await this.httpClient.request(data)
+    const { url, method, body, headers } = data
+    let httpClientData: HttpRequest<any, any> = { url, method, body, headers }
+    const account = this.getStorage.get('account') as Account
+    if (account?.accessToken && account?.name && account?.email) {
+      httpClientData = {
+        ...httpClientData,
+        headers: {
+          'x-access-token': account.accessToken
+        }
+      }
+    }
+    await this.httpClient.request(httpClientData)
 
     return {
       statusCode: 200
