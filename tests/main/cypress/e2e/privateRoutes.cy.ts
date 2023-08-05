e17a13869a8b9ccb6923388997a28b0d87945d7a
Matheus Roika <matheusroika@hotmail.com>
2023-08-05 04:28:44 -0300

    test: ensure privateRoute logouts in SurveyResponse if user has no accessToken

--- tests/main/cypress/e2e/privateRoutes.cy.ts
+++ tests/main/cypress/e2e/privateRoutes.cy.ts
@@ -5,4 +5,9 @@ describe('Private Routes', () => {
     cy.visit('')
     cy.url().should('equal', `${baseUrl}/login`)
   })
+
+  it('Should logout in SurveyResponse if user has no accessToken', () => {
+    cy.visit('/surveys/any_id')
+    cy.url().should('equal', `${baseUrl}/login`)
+  })
 })
