1868106b697e4085bd65056dbb13419584cc89f4
Matheus Roika <matheusroika@hotmail.com>
2023-08-04 23:57:59 -0300

    feat: ensure SurveyResponse page shows error on LoadSurveyResponse UnexpectedError

--- src/presentation/pages/surveyResponse/surveyResponse.tsx
+++ src/presentation/pages/surveyResponse/surveyResponse.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import FlipMove from 'react-flip-move'
 import styles from './surveyResponseStyles.scss'
+import { useErrorHandler } from '@/presentation/hooks/useErrorHandler'
 import Header from '@/presentation/components/header/header'
 import Footer from '@/presentation/components/footer/footer'
 import Loading from '@/presentation/components/loading/loading'
@@ -14,13 +15,21 @@ type Props = {
 }
 
 const SurveyResponse: React.FC<Props> = ({ loadSurveyResponse }) => {
+  const handleError = useErrorHandler((error: Error) => { setError(error.message) })
   const [isLoading] = useState(false)
-  const [error] = useState('')
+  const [error, setError] = useState('')
   const [surveyResponse, setSurveyResponse] = useState<SurveyResponseModel>(null)
 
   const loadAndSetSurveyResponse = async (): Promise<void> => {
-    const surveyResponse = await loadSurveyResponse.load()
-    setSurveyResponse(surveyResponse)
+    try {
+      const surveyResponse = await loadSurveyResponse.load()
+      setSurveyResponse(surveyResponse)
+      setError('')
+    } catch (error) {
+      const typedError = error as Error
+      setSurveyResponse(null)
+      handleError(typedError)
+    }
   }
 
   useEffect(() => {
@@ -32,7 +41,7 @@ const SurveyResponse: React.FC<Props> = ({ loadSurveyResponse }) => {
       <Header />
       <main data-testid="surveyResponse">
         {surveyResponse && <>
-          <hgroup>
+          <hgroup data-testid="title">
             <Calendar date={surveyResponse.survey.date} className={styles.calendar}/>
             <h1 data-testid="question">{surveyResponse.survey.question}</h1>
           </hgroup>
