8c7eed9bb85f00795c5073243e1dea9010909f24
Matheus Roika <matheusroika@hotmail.com>
2023-08-04 23:58:38 -0300

    test: ensure SurveyResponse page shows error on LoadSurveyResponse UnexpectedError

--- tests/presentation/pages/surveyResponse/surveyResponse.spec.tsx
+++ tests/presentation/pages/surveyResponse/surveyResponse.spec.tsx
@@ -3,6 +3,7 @@ import { type RenderResult, render, waitFor } from '@testing-library/react'
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import SurveyResponse from '@/presentation/pages/surveyResponse/surveyResponse'
 import ApiContext from '@/presentation/contexts/apiContext'
+import { UnexpectedError } from '@/domain/errors'
 import { mockAccount, mockSurveyResponse } from '@/../tests/domain/mocks'
 import { mockLoadSurveyResponse } from '@/../tests/data/mocks/mockLoadSurveyResponse'
 import type { SurveyResponse as SurveyResponseModel } from '@/domain/models/SurveyResponse'
@@ -12,9 +13,10 @@ type Sut = {
   loadSpy: jest.SpyInstance<Promise<SurveyResponseModel>, [], any>
 }
 
-const makeSut = (): Sut => {
+const makeSut = (error?: Error): Sut => {
   const loadSurveyResponseStub = mockLoadSurveyResponse()
   const loadSpy = jest.spyOn(loadSurveyResponseStub, 'load')
+  if (error) loadSpy.mockRejectedValueOnce(error)
   const sut = render(
     <ApiContext.Provider value={{ getCurrentAccount: () => mockAccount(), setCurrentAccount: jest.fn() }}>
       <MemoryRouter initialEntries={['/survey']}>
@@ -75,6 +77,19 @@ describe('Survey Response Page', () => {
     expect(percents[0].textContent).toBe(`${surveyResponse.survey.answers[0].percent}%`)
     expect(percents[1].textContent).toBe(`${surveyResponse.survey.answers[1].percent}%`)
 
+    expect(sut.queryByTestId('loading')).toBeNull()
+    expect(sut.queryByTestId('error')).toBeNull()
     expect(sut.queryByTestId('back')).toBeTruthy()
+  })
+
+  test('Should show error on LoadSurveyResponse UnexpectedError', async () => {
+    const error = new UnexpectedError()
+    const { sut, loadSpy } = makeSut(error)
+    await waitFor(() => sut.getByTestId('surveyResponse'))
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(sut.queryByTestId('title')).toBeNull()
+    expect(sut.queryByTestId('answers')).toBeNull()
+    expect(sut.queryByTestId('loading')).toBeNull()
+    expect(sut.queryByTestId('error').textContent).toBe(error.message)
   })
 })
