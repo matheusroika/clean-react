b70c6e19d56e9527a2c35ab0c9b937277a1c5ab0
Matheus Roika <matheusroika@hotmail.com>
2023-08-05 22:38:56 -0300

    fix: change e2e test to reflect ordering answers by percent

--- tests/main/cypress/e2e/surveyResponse.cy.ts
+++ tests/main/cypress/e2e/surveyResponse.cy.ts
@@ -47,15 +47,15 @@ describe('SurveyResponse', () => {
       cy.dataTestId('year').should('have.text', '2023')
       cy.dataTestId('question').should('have.text', 'any_question')
       cy.get('li:first-child').then(li => {
-        assert.notExists(li.find('[data-testid="image"]'))
-        assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
-        assert.equal(li.find('[data-testid="percent"]').text(), '0%')
-      })
-      cy.get('li:last-child').then(li => {
         cy.wrap(li).should('have.css', 'box-shadow', 'rgb(188, 71, 120) 0px 0px 3px 2px')
         assert.equal(li.find('[data-testid="image"]').attr('src'), 'image.png')
         assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer')
         assert.equal(li.find('[data-testid="percent"]').text(), '100%')
+      })
+      cy.get('li:last-child').then(li => {
+        assert.notExists(li.find('[data-testid="image"]'))
+        assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
+        assert.equal(li.find('[data-testid="percent"]').text(), '0%')
       })
     })
 
@@ -96,27 +96,27 @@ describe('SurveyResponse', () => {
 
     it('Should show surveyResponse correctly', () => {
       http.mockOk()
-      cy.get('li:first-child').click()
+      cy.get('li:last-child').click()
       cy.dataTestId('day').should('have.text', '01')
       cy.dataTestId('month').should('have.text', 'ago')
       cy.dataTestId('year').should('have.text', '2023')
       cy.dataTestId('question').should('have.text', 'other_question')
       cy.get('li:first-child').then(li => {
+        assert.equal(li.find('[data-testid="image"]').attr('src'), 'image.png')
+        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer')
+        assert.equal(li.find('[data-testid="percent"]').text(), '80%')
+      })
+      cy.get('li:last-child').then(li => {
         cy.wrap(li).should('have.css', 'box-shadow', 'rgb(188, 71, 120) 0px 0px 3px 2px')
         assert.notExists(li.find('[data-testid="image"]'))
         assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
         assert.equal(li.find('[data-testid="percent"]').text(), '20%')
       })
-      cy.get('li:last-child').then(li => {
-        assert.equal(li.find('[data-testid="image"]').attr('src'), 'image.png')
-        assert.equal(li.find('[data-testid="answer"]').text(), 'other_answer')
-        assert.equal(li.find('[data-testid="percent"]').text(), '80%')
-      })
     })
 
     it('Should retry to load SurveyResponse on button click', () => {
       http.mockUnexpectedError()
-      cy.get('li:first-child').click()
+      cy.get('li:last-child').click()
       cy.dataTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente')
       http.mockOk()
       cy.dataTestId('retry').click()
@@ -125,13 +125,13 @@ describe('SurveyResponse', () => {
 
     it('Should show error message on UnexpectedError', () => {
       http.mockUnexpectedError()
-      cy.get('li:first-child').click()
+      cy.get('li:last-child').click()
       cy.dataTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente')
     })
 
     it('Should logout on AccessDeniedError', () => {
       http.mockAccessDeniedError()
-      cy.get('li:first-child').click()
+      cy.get('li:last-child').click()
       cy.url().should('equal', `${baseUrl}/login`).then(() => {
         expect(localStorage.getItem('account')).to.be.a('null')
       })
